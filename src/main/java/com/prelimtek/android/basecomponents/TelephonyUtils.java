92eb69d00d85b45862312dc54e1a66fc86ae0aef
kaniundungu <kaniu@mtini.io>
2019-08-02 01:02:00 -0700

    removed sdk restriction; fixed whatsapp calling

--- src/main/java/com/prelimtek/android/basecomponents/TelephonyUtils.java
+++ src/main/java/com/prelimtek/android/basecomponents/TelephonyUtils.java
@@ -22,7 +22,7 @@ public class TelephonyUtils {
 
 
     public static void makePhoneCall(Activity context, String phoneNumber){
-        if (Build.VERSION.SDK_INT > 22) {
+        //if (Build.VERSION.SDK_INT > 22) {
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) !=
                     PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_REQ_CODE);
@@ -30,17 +30,23 @@ public class TelephonyUtils {
                 return;
             }
 
+
+        try{
             //intent call
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(Uri.parse("tel:+" + phoneNumber));
             context.startActivityForResult(callIntent,PHONE_CALL_REQ_CODE);//a response is sent to onRequestPermissionsResult mthd
+        }catch(Exception e){
+            Log.e(TAG,"Make phone call failed.",e);
+            DialogUtils.startErrorDialog(context,"Make phone call failed.");
         }
+                    //}
     }
 
 
     public static void composeSMSMessage(Activity context, String phoneNumber){
 
-        if (Build.VERSION.SDK_INT > 22) {
+        //if (Build.VERSION.SDK_INT > 22) {
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) !=
                     PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.SEND_SMS}, TEXT_MESG_REQ_CODE);
@@ -48,18 +54,22 @@ public class TelephonyUtils {
                 return;
             }
 
-            Intent callIntent = new Intent(Intent.ACTION_VIEW);
-            callIntent.setData(Uri.parse("sms:"+phoneNumber));
-            context.startActivityForResult(callIntent,TEXT_MESG_REQ_CODE );//a response is sent to onRequestPermissionsResult mthd
-
-        }
+            try{
+                Intent callIntent = new Intent(Intent.ACTION_VIEW);
+                callIntent.setData(Uri.parse("sms:"+phoneNumber));
+                context.startActivityForResult(callIntent,TEXT_MESG_REQ_CODE );//a response is sent to onRequestPermissionsResult mthd
+            }catch(Exception e){
+             Log.e(TAG,"Compose sms failed.",e);
+                DialogUtils.startErrorDialog(context,"Compose SMS failed.");
+            }
+        //}
 
     }
 
 
     public static void composeWhatsappMessage(Activity context, String phoneNumber){
         //intent call
-        if (Build.VERSION.SDK_INT > 22) {
+        //if (Build.VERSION.SDK_INT > 22) {
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) !=
                     PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.SEND_SMS}, WHATSAPP_MESG_REQ_CODE);
@@ -68,8 +78,8 @@ public class TelephonyUtils {
             }
 
             try {
-                Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
-                whatsappIntent.setData(Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
+                Intent whatsappIntent = new Intent(Intent.ACTION_SENDTO);
+                whatsappIntent.setData(Uri.parse("smsto:" + phoneNumber));
                 whatsappIntent.setPackage("com.whatsapp");
                 //context.startActivity(Intent.createChooser(whatsappIntent,""));
                 context.startActivityForResult(whatsappIntent, WHATSAPP_MESG_REQ_CODE);//a response is sent to onRequestPermissionsResult mthd
@@ -77,6 +87,6 @@ public class TelephonyUtils {
                 Log.e(TAG,"App not found .",e);
                 DialogUtils.startErrorDialog(context,"Please install Whatsapp app and try again.");
             }
-        }
+       // }
     }
 }
