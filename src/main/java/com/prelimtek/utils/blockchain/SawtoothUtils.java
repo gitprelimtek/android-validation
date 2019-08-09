36e166703467db0c3c36b8a9bb23d66cec4155a0
kaniundungu <kaniu@mtini.io>
2019-08-08 19:24:11 -0700

    Remove utf-8 charset; replace with default charset due to SecurityExctn

--- src/main/java/com/prelimtek/utils/blockchain/SawtoothUtils.java
+++ src/main/java/com/prelimtek/utils/blockchain/SawtoothUtils.java
@@ -39,7 +39,10 @@ public class SawtoothUtils {
 	public static final String VERSION= "1.0";
 
 	public static String calculateAddress(String familyName, byte[] publicKey) throws UnsupportedEncodingException{
-		return Utils.hash512(familyName.getBytes(Charsets.UTF_8)).substring(0, 6)
+		//return Utils.hash512(familyName.getBytes(Charsets.UTF_8)).substring(0, 6)
+		//+ Utils.hash512(publicKey).substring(0, 64);
+		
+		return Utils.hash512(familyName.getBytes()).substring(0, 6)
 		+ Utils.hash512(publicKey).substring(0, 64);
 	}
 
