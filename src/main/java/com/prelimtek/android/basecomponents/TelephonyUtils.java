a6606226faf54a6fe19a58745f32900174434e24
kaniundungu <kaniu@mtini.io>
2019-09-04 14:56:19 -0700

    Merge branch 'Release_1.1.0-SNAPSHOT' of https://github.com/gitprelimtek/android-base-components into Release_1.1.0-full-SNAPSHOT

--- src/main/java/com/prelimtek/android/basecomponents/TelephonyUtils.java
+++ src/main/java/com/prelimtek/android/basecomponents/TelephonyUtils.java
@@ -20,15 +20,15 @@ public class TelephonyUtils {
     public static final int TEXT_MESG_REQ_CODE = 11112;
     public static final int WHATSAPP_MESG_REQ_CODE = 11113;
 
-
-    public static void makePhoneCall(Activity context, String phoneNumber){
+    @Deprecated
+    public static void makePhoneCall1(Activity context, String phoneNumber){
         //if (Build.VERSION.SDK_INT > 22) {
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) !=
-                    PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_REQ_CODE);
-                Log.w(TAG,"Requesting Permission CALL_PHONE .");
-                return;
-            }
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_REQ_CODE);
+            Log.w(TAG,"Requesting Permission CALL_PHONE .");
+            return;
+        }
 
 
         try{
@@ -40,53 +40,99 @@ public class TelephonyUtils {
             Log.e(TAG,"Make phone call failed.",e);
             DialogUtils.startErrorDialog(context,"Make phone call failed.");
         }
-                    //}
+        //}
+    }
+
+    @Deprecated
+    public static void composeSMSMessage1(Activity context, String phoneNumber){
+
+        //if (Build.VERSION.SDK_INT > 22) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.SEND_SMS}, TEXT_MESG_REQ_CODE);
+            Log.w(TAG,"Requesting Permission SEND_SMS .");
+            return;
+        }
+
+        try{
+            Intent callIntent = new Intent(Intent.ACTION_VIEW);
+            callIntent.setData(Uri.parse("sms:"+phoneNumber));
+            context.startActivityForResult(callIntent,TEXT_MESG_REQ_CODE );//a response is sent to onRequestPermissionsResult mthd
+        }catch(Exception e){
+            Log.e(TAG,"Compose sms failed.",e);
+            DialogUtils.startErrorDialog(context,"Compose SMS failed.");
+        }
+        //}
+
+    }
+
+    @Deprecated
+    public static void composeWhatsappMessage1(Activity context, String phoneNumber){
+        //intent call
+        //if (Build.VERSION.SDK_INT > 22) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.SEND_SMS}, WHATSAPP_MESG_REQ_CODE);
+            Log.w(TAG,"Requesting Permission SEND_SMS .");
+            return;
+        }
+
+        try {
+            Intent whatsappIntent = new Intent(Intent.ACTION_SENDTO);
+            whatsappIntent.setData(Uri.parse("smsto:" + phoneNumber));
+            whatsappIntent.setPackage("com.whatsapp");
+            //context.startActivity(Intent.createChooser(whatsappIntent,""));
+            context.startActivityForResult(whatsappIntent, WHATSAPP_MESG_REQ_CODE);//a response is sent to onRequestPermissionsResult mthd
+        }catch(ActivityNotFoundException e){
+            Log.e(TAG,"App not found .",e);
+            DialogUtils.startErrorDialog(context,"Please install Whatsapp app and try again.");
+        }
+        // }
+    }
+
+
+    public static void makePhoneCall(Activity context, String phoneNumber){
+
+        try{
+            //intent call
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:+" + phoneNumber));
+            context.startActivityForResult(callIntent,PHONE_CALL_REQ_CODE);//a response is sent to onRequestPermissionsResult mthd
+        }catch(Exception e){
+            Log.e(TAG,"Make phone call failed.",e);
+            DialogUtils.startErrorDialog(context,"Make phone call failed.");
+        }
+        //}
     }
 
 
     public static void composeSMSMessage(Activity context, String phoneNumber){
 
-        //if (Build.VERSION.SDK_INT > 22) {
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) !=
-                    PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.SEND_SMS}, TEXT_MESG_REQ_CODE);
-                Log.w(TAG,"Requesting Permission SEND_SMS .");
-                return;
-            }
-
-            try{
-                Intent callIntent = new Intent(Intent.ACTION_VIEW);
-                callIntent.setData(Uri.parse("sms:"+phoneNumber));
-                context.startActivityForResult(callIntent,TEXT_MESG_REQ_CODE );//a response is sent to onRequestPermissionsResult mthd
-            }catch(Exception e){
-             Log.e(TAG,"Compose sms failed.",e);
-                DialogUtils.startErrorDialog(context,"Compose SMS failed.");
-            }
+        try{
+            Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+            smsIntent.setData(Uri.parse("smsto:" + phoneNumber));
+            context.startActivityForResult(smsIntent,TEXT_MESG_REQ_CODE );//a response is sent to onRequestPermissionsResult mthd
+        }catch(Exception e){
+            Log.e(TAG,"Compose sms failed.",e);
+            DialogUtils.startErrorDialog(context,"Compose SMS failed.");
+        }
         //}
 
     }
 
 
     public static void composeWhatsappMessage(Activity context, String phoneNumber){
-        //intent call
-        //if (Build.VERSION.SDK_INT > 22) {
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) !=
-                    PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.SEND_SMS}, WHATSAPP_MESG_REQ_CODE);
-                Log.w(TAG,"Requesting Permission SEND_SMS .");
-                return;
-            }
 
-            try {
-                Intent whatsappIntent = new Intent(Intent.ACTION_SENDTO);
-                whatsappIntent.setData(Uri.parse("smsto:" + phoneNumber));
-                whatsappIntent.setPackage("com.whatsapp");
-                //context.startActivity(Intent.createChooser(whatsappIntent,""));
-                context.startActivityForResult(whatsappIntent, WHATSAPP_MESG_REQ_CODE);//a response is sent to onRequestPermissionsResult mthd
-            }catch(ActivityNotFoundException e){
-                Log.e(TAG,"App not found .",e);
-                DialogUtils.startErrorDialog(context,"Please install Whatsapp app and try again.");
-            }
-       // }
+        try {
+            Intent whatsappIntent = new Intent(Intent.ACTION_SENDTO);
+            whatsappIntent.setData(Uri.parse("smsto:" + phoneNumber));
+            whatsappIntent.setPackage("com.whatsapp");
+            //context.startActivity(Intent.createChooser(whatsappIntent,""));
+            context.startActivityForResult(whatsappIntent, WHATSAPP_MESG_REQ_CODE);//a response is sent to onRequestPermissionsResult mthd
+        }catch(ActivityNotFoundException e){
+            Log.e(TAG,"App not found .",e);
+            DialogUtils.startErrorDialog(context,"Please install Whatsapp app and try again.");
+        }
+        // }
     }
 }
---
+++ src/main/java/com/prelimtek/android/customcomponents/NotesTextRecyclerViewAdapter.java
@@ -0,0 +1,133 @@
+package com.prelimtek.android.customcomponents;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.prelimtek.android.basecomponents.Configuration;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class NotesTextRecyclerViewAdapter extends RecyclerView.Adapter<NotesTextRecyclerViewAdapter.ViewHolder> {
+
+    public static final int PAGE_BUFFER_SIZE = 3;
+    public static final int PAGE_VISIBLE_SIZE = 2;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView noteDateTextView;
+        TextView noteTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            noteDateTextView = (TextView) itemView.findViewById(R.id.notedate);
+            noteTextView = (TextView)itemView.findViewById(R.id.noteText);
+            itemView.setOnClickListener(onClickListener);
+            itemView.setFocusable(true);
+            //itemView.setTag(noteDateTextView.getTag());
+        }
+
+        public void bindTo(NotesModel notesModel) {
+            setDateValue(noteDateTextView,notesModel.getDate());
+            noteTextView.setText(notesModel.noteText);
+            noteTextView.setTag(notesModel.getDate());
+        }
+
+        public void clear() {
+            noteDateTextView.setText(null);
+            noteDateTextView.setTag(null);
+            noteTextView.setText(null);
+            noteTextView.setTag(null);
+        }
+
+        //TODO put in util class
+        public void setDateValue(TextView view,
+                                        final Long newDate ){
+            view.setTag(newDate);
+            DateFormat dateFormat = Configuration.configuredPreferences(view.getContext()).dateFormat;
+            if(newDate!=null) {
+                String newDateStr = dateFormat.format(new Date(newDate));
+                view.setText(newDateStr);
+            }
+
+        }
+
+
+    }
+
+    Context context;
+    List<NotesModel> rowItems;
+    View.OnClickListener onClickListener;
+    int layoutId = -1;
+    LayoutInflater inflator = null;
+    //MediaDAOInterface db = null;
+
+
+
+    public NotesTextRecyclerViewAdapter(@NonNull Context context, @NonNull List<NotesModel> items, int layoutId, @NonNull View.OnClickListener onClickListener) {
+        this.context = context;
+        this.rowItems = items;
+        this.onClickListener = onClickListener;
+        this.layoutId=layoutId;
+        this.inflator = LayoutInflater.from(context);
+        //this.db = dbHelper;
+        /*try {
+            //TODO make this cache
+            db =  AppDAO.builder().open(context);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return rowItems==null?0:rowItems.size();
+    }
+
+    public void setRowItems(List<NotesModel> rowItems) {
+        this.rowItems = rowItems;
+    }
+
+    public void appendRowItems(List<NotesModel> rowItems) {
+        this.rowItems.addAll(rowItems);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View convertView = inflator.inflate(R.layout.notes_list_layout,parent,false);
+
+        ViewHolder holder = new NotesTextRecyclerViewAdapter.ViewHolder(convertView);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        NotesModel notesModel = rowItems.get(position);
+
+        if(notesModel!=null)
+            holder.bindTo(notesModel);
+        else
+            holder.clear();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    public NotesModel getItem(int position) {
+        return rowItems.get(position);
+    }
+
+
+}
