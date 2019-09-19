5c73fc0c79d14e41ac3593427387ac4d279f0ee1
kaniundungu <kaniu@mtini.io>
2019-09-19 13:18:04 -0700

    replaced add method

--- src/main/java/com/prelimtek/android/customcomponents/TextDAOInterface.java
+++ src/main/java/com/prelimtek/android/customcomponents/TextDAOInterface.java
@@ -5,11 +5,6 @@ import java.util.List;
 public interface TextDAOInterface {
 
     public boolean addNotes(NotesModel notes) throws Exception;
-<<<<<<< HEAD
-=======
-  
-    public boolean addNotes(String modelId, String noteText);
->>>>>>> 42e644bd127b7034aec76905b0e1cf8b03f80012
 
     public NotesModel[] getNotes(String modelId, Long afterThisDate, int rowCount, int pageOffset);
 
