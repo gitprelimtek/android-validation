c6c55ebda1439b8621a95306dc7335867a878c9f
kaniundungu <kaniu@mtini.io>
2019-11-14 19:56:42 -0800

    Merge branch 'Release_1.1.0-SNAPSHOT' of https://github.com/gitprelimtek/crypto-utils into 0.0.1-SNAPSHOT-ptek-1-1-0

--- src/main/java/com/prelimtek/utils/crypto/dao/CryptoDAOInterface.java
+++ src/main/java/com/prelimtek/utils/crypto/dao/CryptoDAOInterface.java
@@ -3,14 +3,24 @@ package com.prelimtek.utils.crypto.dao;
 import java.io.IOException;
 import java.io.Serializable;
 
-public interface CryptoDAOInterface<T1,T2>  extends Serializable {
+public interface CryptoDAOInterface  extends Serializable {
 
-    public String getPrivateKey(String id);
+     public interface LocalDAO <T1,T2>extends Serializable{
+         String getPrivateKey(String id);
 
-    public String updatePrivateKey(String id, String privateKey);
+         String updatePrivateKey(String id, String privateKey);
 
-    public void updateWallet(String address, String oldAddress, T1 securityModel, T2 wallet, String action) throws IOException;
+         void updateWallet(String address, String oldAddress, T1 securityModel, T2 wallet, String action) throws IOException;
 
-    public T2 getMyWallet(String address, T1 securityModel) throws IOException;
+         T2 getMyWallet(String address, T1 securityModel) throws IOException;
 
+
+     }
+
+     public interface RemoteDAO<T1,T2>extends Serializable{
+         T2 retrieveMyWallet(String address, T1 sModel,  String... jwt) throws Exception ;
+
+         boolean updateWallet(String address, String oldAddress, T1 sModel, T2 wallet, String action,  String... jwt) throws Exception;
+
+     }
 }
