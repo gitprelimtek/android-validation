2d1ae427f7244a7da3ba59ac758713ae08a03e4f
kaniundungu <kaniu@mtini.io>
2019-11-14 20:49:17 -0800

    moving to android-auth-components for remote authentication

--- src/main/java/com/ptek/android/auth/AuthRemoteDAOInterface.java
+++ src/main/java/com/ptek/android/auth/AuthRemoteDAOInterface.java
@@ -2,8 +2,11 @@ package com.ptek.android.auth;
 
 import android.support.annotation.Nullable;
 
+import com.prelimtek.android.basecomponents.dao.RemoteDAOListener;
 import com.prelimtek.utils.crypto.Wallet;
 import com.prelimtek.utils.crypto.dao.CryptoDAOInterface;
+
+import java.io.IOException;
 
 public interface AuthRemoteDAOInterface<T1,T2> extends CryptoDAOInterface.RemoteDAO<T1,T2> {
 
@@ -11,12 +14,12 @@ public interface AuthRemoteDAOInterface<T1,T2> extends CryptoDAOInterface.Remote
     //Wallet retrieveMyWallet(String address, AuthModel sModel, @Nullable  String ... jwt) throws RemoteAuthException ;
     //crypto
     //boolean updateWallet(String address, String oldAddress, AuthModel sModel, Wallet wallet, String action, @Nullable String ... jwt) throws RemoteAuthException ;
-
-    void requestEmailTextToken(T1 sModel, Object listener) throws RemoteAuthException;
+    /**Explicitly expect RemoteDAOListener*/
+    void requestEmailTextToken(T1 sModel, RemoteDAOListener<?> listener) throws RemoteAuthException;
 
     String retrieveServerSigner(T1 sModel) throws RemoteAuthException;
 
-    public class RemoteAuthException extends Exception{
+    public class RemoteAuthException extends IOException {
         public RemoteAuthException(String s){super(s);}
         public RemoteAuthException(Throwable e){super(e);}
     }
